4707b15369cd34ab98379ea96f47ead89b78f93f
Esteban Acevedo <esteban.horacio.acevedo@gmail.com>
2025-05-20 20:54:49 -0300

    Agregar views de pruebas

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saludar/', views.saludar),
+    path('saludar2/', views.saludar_con_etiqueta),
+    path('saludar3/<str:nombre>/<str:apellido>', views.saludar_con_parametros),
 ]
